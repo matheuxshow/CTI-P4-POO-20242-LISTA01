0dd2eadfded47adfebe15a18c06fa8df2e7e219a
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 16:54:21 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE23/src/br/edu/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double num, i , f, a;
+		num = 22.17;
+		i = (int)num;
+		f = num - i;
+		a = Math.round(num);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(a);
+		
+		
+		
+
+	}
+
+}
