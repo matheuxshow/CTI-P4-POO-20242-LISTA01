b4ce52dbe6ba6cf7037d748d340a1e0acc86beb1
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:43:10 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double altura = 22;
+		double base = 17;
+		double area = (altura * base) /2;
+		System.out.println("a area do triangulo é " + area);
+	}
+
+}
