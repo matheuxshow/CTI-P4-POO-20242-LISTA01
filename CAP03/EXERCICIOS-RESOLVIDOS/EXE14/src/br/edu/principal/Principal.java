b71b055f2dafad489a3044e1f65dcfb5eec38f00
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 16:51:30 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE14/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int ano_nasc= 2022;
+		int ano_atual= 2024;
+		int idade_hj = (ano_atual - ano_nasc);
+		int idade_2050 = ( 2050 - ano_nasc);
+		System.out.println(idade_hj);
+		System.out.println(idade_2050);
+
+	}
+
+}
