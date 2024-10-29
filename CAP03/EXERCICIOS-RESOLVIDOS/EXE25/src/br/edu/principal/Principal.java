a67c8a51fb494a165c1ed6437cdb57707823d541
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 16:54:59 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE25/src/br/edu/principal/Principal.java
@@ -0,0 +1,16 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double custo, convite, qtd;
+		custo = 17.22;
+		convite = 22.17;
+		qtd = custo / convite;
+		System.out.println(qtd);
+		
+		
+
+	}
+
+}
