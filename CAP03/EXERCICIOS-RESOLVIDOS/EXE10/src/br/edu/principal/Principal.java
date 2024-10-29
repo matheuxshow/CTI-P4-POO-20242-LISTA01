7b56ae58b83b627567c30f44197e9753a228f734
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:43:25 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE10/src/br/edu/principal/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double pi = 3.1415;
+		double raio = 22;
+		System.out.print("a area é " + ( pi*raio*raio));
+	}
+
+}
