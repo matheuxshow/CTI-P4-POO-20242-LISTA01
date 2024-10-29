9f801fd0c7f4318509fd8d854e4959a9ced86072
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:38:13 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE05/src/exbr/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package exbr.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double salario = 1416.00;
+		double percent= 0.06 ;
+		double aumento = salario * percent;
+				System.out.println("o aumento é de: " + aumento );
+				
+				System.out.print("o novo salario é: "+ (salario+aumento) );
+		
+	}
+
+}
