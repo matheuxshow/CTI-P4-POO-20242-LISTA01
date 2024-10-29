9f1c497d5fcea89b775b44fb6b448fb72be63cda
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:43:43 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE11/src/exbr/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package exbr.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double n = 1;
+		System.out.println(Math.pow(n, 2));
+		System.out.println(Math.pow(n, 3));
+		System.out.println(Math.pow(n, 1/2));
+		System.out.println(Math.pow(n, 1/3));
+	}
+
+}
