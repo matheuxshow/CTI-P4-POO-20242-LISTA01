298174463340de6257e83a088bb1e76db716de71
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 16:53:16 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE19/src/br/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double a_degrau= 17;
+		double a_usuario = 171;
+		double qdegraus= a_usuario / a_degrau;
+		System.out.print(qdegraus);
+
+	}
+
+}
