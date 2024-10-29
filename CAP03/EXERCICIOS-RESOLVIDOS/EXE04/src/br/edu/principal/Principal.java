5d836d8392f8b8d3d8fbec9673a6062db9c0667a
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:37:41 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double sal = 1416.00;
+		double novosal = sal * 1.25;
+		 System.out.print("o novo salário é: "  + novosal);
+		 
+	}
+
+}
