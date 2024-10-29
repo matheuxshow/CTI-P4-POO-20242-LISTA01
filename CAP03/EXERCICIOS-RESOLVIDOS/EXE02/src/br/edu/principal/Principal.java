bd32d760cccf707f7e522a842c8f2022c2605e2c
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:36:51 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE02/src/br/edu/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float n1= 7;
+		float n2= 7;
+		float n3= 7;
+		float media = (n1+n2+n3)/3;
+		System.out.print(media);
+	}
+
+}
