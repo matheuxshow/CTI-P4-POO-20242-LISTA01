80f0e8a602a9800af022ae01db954f873def0f54
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:37:19 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE03/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		float n1 = 5;
+		float n2 = 8;
+		float n3 = 10;
+		float p1 = 2;
+		float p2 = 3;
+		float p3 = 4;
+		float media = (n1*p1+n2*p2+n3*p3)/(p1+p2+p3);
+		System.out.print(media);
+
+	}
+
+}
