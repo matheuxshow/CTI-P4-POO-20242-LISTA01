09f2f43382607964f94af58b70cb825ec6bbbea0
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:32:07 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE01/src/br/edu/principal/Principal.java
@@ -0,0 +1,16 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1= 3;
+		int n2= 3;
+		int n3= 3;
+		int n4= 3;
+		int soma = n1 + n2 +n3 +n4;
+		System.out.print(soma);
+		
+	}
+
+}
