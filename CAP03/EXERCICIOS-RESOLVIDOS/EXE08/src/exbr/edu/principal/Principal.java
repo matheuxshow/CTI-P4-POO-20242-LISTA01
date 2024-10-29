6d7d1b408b599237320278d5148d486bcfaa3b70
matheuxshow <152522448+matheuxshow@users.noreply.github.com>
2024-10-29 14:42:54 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE08/src/exbr/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package exbr.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double deposito = 1416.00;
+		double taxa = 0.00000000000000000000000000000000000000001;
+		double rend = deposito * taxa;
+		double salariof = deposito + rend;
+			System.out.println(rend);
+		System.out.print(salariof);
+
+	}
+
+}
---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE08/src/exbr/edu/principal/package-info.java
@@ -0,0 +1,1 @@
+package exbr.edu.principal;
